ca56b8cda0cda9c304f86bb92d8c1527f3541e70
Abhishek Kumar <36950025+Abhishekkumar2k18@users.noreply.github.com>
2018-03-09 10:05:57 -0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,88 +1,117 @@
-from Tkinter import Tk
-from Tkinter import StringVar,Entry,Button
-from math import pi,e,sin,cos,tan,log,log10,ceil,degrees,radians,exp,asin,acos,floor
+from tkinter import *
 
 
-class calculator:
+class calculate():
+
     def __init__(self):
-        window=Tk()
-        window.title('Scientific Calculator')
-        window.configure(background="white")
-        self.string=StringVar()
-        entry=Entry(window,textvariable=self.string)
-        entry.grid(row=0,column=0,columnspan=6)
-        entry.configure(background="white")
-        entry.focus()
-        
-        values=["7","8","9","/","%","clear","AC",
-                "4","5","6","*","(",")","**",
-                "1","2","3","-","=",",","0",".","min","+","sin","asin","cos","acos","tan()",
-                "pow","log10","max","abs","floor","pi","e","log","ceil","degrees","radians"]
-        text=1
-        i=0
-        row=1
-        col=0
-        for txt in values:
-            padx=10
-            pady=10
-            if(i==7):
-                row=2
-                col=0
-            if(i==14):
-                row=3
-                col=0
-            if(i==19):
-                row=4
-                col=0
-            if(i==26):
-                row=5
-                col=0
-            if(i==33):
-                row=6
-                col=0
-            if(txt=='='):
-                btn=Button(window,height=2,width=4,padx=70,pady=pady,text=txt,command=lambda txt=txt:self.equals())
-                btn.grid(row=row,column=col,columnspan=3,padx=2,pady=2)
-                btn.configure(background="yellow")
+        self.root = Tk()
+        self.root.title("Calculator")
+        self.root.geometry("370x220")
 
-            elif(txt=='clear'):
-                btn=Button(window,height=2,width=4,padx=padx,pady=pady, text=txt ,command=lambda txt=txt:self.delete())
-                btn.grid(row=row,column=col,padx=1,pady=1)
-                btn.configure(background="grey")
-            elif(txt=='AC'):
-                btn=Button(window,height=2,width=4,padx=padx,pady=pady,text=txt,command=lambda txt=txt:self.clearall())
-                btn.grid(row=row,column=col,padx=1,pady=1)
-                btn.configure(background="red")
-            else:
-                btn=Button(window,height=2,width=4,padx=padx,pady=pady,text=txt ,command=lambda txt=txt:self.addChar(txt))
-                btn.grid(row=row,column=col,padx=1,pady=1)
-                btn.configure(background="cyan")
+        self.resultwindow = Entry(self.root)
+        self.resultwindow.grid(row=0,column=0,columnspan=6)
+        self.resultwindow.config(font=("Arial", 18))
+        self.resultwindow.focus_set()  # Sets focus on the input text area
 
-            col=col+1
-            i=i+1
-        window.mainloop()
-        
+        # Buttons
+        self.button1 = Button(self.root, text="1", width=3, command=lambda:self.ins('1'))
+        self.button1.grid(row=1,column=0, padx=3, pady=3)
+        self.button1.config(font=("Arial", 18))
 
-    def clearall(self):
-        self.string.set("")
+        self.button2 = Button(self.root, text="2", width=3, command=lambda:self.ins('2'))
+        self.button2.grid(row=1, column=1, padx=3, pady=3)
+        self.button2.config(font=("Arial", 18))
 
-    def equals(self):
-        result=""
+        self.button3 = Button(self.root, text="3", width=3, command=lambda:self.ins('3'))
+        self.button3.grid(row=1, column=2, padx=3, pady=3)
+        self.button3.config(font=("Arial", 18))
 
-        try:
-            result=eval(self.string.get())
-            self.string.set(result)
-        except:
-            result="INVALID INPUT"
-        self.string.set(result)
-        
-    def addChar(self,char):
-        self.string.set(self.string.get()+(str(char)))
-        
-    def delete(self):
-        self.string.set(self.string.get()[0:-1])
-           
-calculator()
+        self.button4 = Button(self.root, text="4", width=3, command=lambda:self.ins('4'))
+        self.button4.grid(row=2, column=0, padx=3, pady=3)
+        self.button4.config(font=("Arial", 18))
 
-  
-        
+        self.button5 = Button(self.root, text="5", width=3, command=lambda:self.ins('5'))
+        self.button5.grid(row=2, column=1, padx=3, pady=3)
+        self.button5.config(font=("Arial", 18))
+
+        self.button6 = Button(self.root, text="6", width=3, command=lambda:self.ins('6'))
+        self.button6.grid(row=2, column=2, padx=3, pady=3)
+        self.button6.config(font=("Arial", 18))
+
+        self.button7 = Button(self.root, text="7", width=3, command=lambda:self.ins('7'))
+        self.button7.grid(row=3, column=0, padx=3, pady=3)
+        self.button7.config(font=("Arial", 18))
+
+        self.button8 = Button(self.root, text="8", width=3, command=lambda:self.ins('8'))
+        self.button8.grid(row=3, column=1, padx=3, pady=3)
+        self.button8.config(font=("Arial", 18))
+
+        self.button9 = Button(self.root, text="9", width=3, command=lambda:self.ins('9'))
+        self.button9.grid(row=3, column=2, padx=3, pady=3)
+        self.button9.config(font=("Arial", 18))
+
+        self.button0 = Button(self.root, text="0", width=3, command=lambda: self.ins('0'))
+        self.button0.grid(row=4, column=0, padx=3, pady=3)
+        self.button0.config(font=("Arial", 18))
+
+        self.button_open = Button(self.root, text="(", width=3, command=lambda: self.ins('('))
+        self.button_open.grid(row=4, column=1, padx=3, pady=3)
+        self.button_open.config(font=("Arial", 18))
+
+        self.button_close = Button(self.root, text=")", width=3, command=lambda: self.ins(')'))
+        self.button_close.grid(row=4, column=2, padx=3, pady=3)
+        self.button_close.config(font=("Arial", 18))
+
+        # Operations Buttons
+
+        self.buttonplus = Button(self.root, text="+", width=3, command=lambda:self.ins('+'))
+        self.buttonplus.grid(row=1, column=3, padx=3, pady=3)
+        self.buttonplus.config(font=("Arial", 18))
+
+        self.buttonminus = Button(self.root, text="-", width=3, command=lambda:self.ins('-'))
+        self.buttonminus.grid(row=1, column=4, padx=3, pady=3)
+        self.buttonminus.config(font=("Arial", 18))
+
+        self.buttondivide = Button(self.root, text="/", width=3, command=lambda:self.ins('/'))
+        self.buttondivide.grid(row=2, column=3, padx=3, pady=3)
+        self.buttondivide.config(font=("Arial", 18))
+
+        self.buttonmultiply = Button(self.root, text="*", width=3, command=lambda:self.ins('*'))
+        self.buttonmultiply.grid(row=2, column=4, padx=3, pady=3)
+        self.buttonmultiply.config(font=("Arial", 18))
+
+        self.buttoncancel = Button(self.root, text="C", width=3, command=lambda: self.cancel())
+        self.buttoncancel.grid(row=3, column=4, padx=3, pady=3)
+        self.buttoncancel.config(font=("Arial", 18))
+
+        self.buttondeleteall = Button(self.root, text="Del", width=3, command=lambda: self.delete_all())
+        self.buttondeleteall.grid(row=3, column=3, padx=3, pady=3)
+        self.buttondeleteall.config(font=("Arial", 18))
+
+        self.buttonresult = Button(self.root, text="=", width=6, command=lambda:self.calculate())
+        self.buttonresult.grid(row=4, column=3, padx=3, pady=3, columnspan=2)
+        self.buttonresult.config(font=("Arial", 18))
+
+        self.root.mainloop()
+
+    def ins(self,val):
+        self.resultwindow.insert(END, val)
+
+    def cancel(self):
+        self.resultwindow.delete(0, 'end')
+
+    def delete_all(self):
+        x = self.resultwindow.get()
+        self.resultwindow.delete(0, 'end')
+        y = x[:-1]
+        self.resultwindow.insert(0, y)
+
+    def calculate(self):
+        x = self.resultwindow.get()
+        answer = eval(x)
+        self.resultwindow.delete(0, 'end')
+        self.resultwindow.insert(0, answer)
+
+
+if __name__ == "__main__":
+    calculate()
