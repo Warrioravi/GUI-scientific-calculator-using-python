14e4a6b2a0673fc259958ea13fc7fa6fa7ee20ef
sharklab7 <55528399+sharklab7@users.noreply.github.com>
2020-10-01 17:00:08 +0530

    Update calculator.py
    1. Layout Fixed
    2. Default Layout is Set.
    3. Users can't change the size of the output window.
    4. Appearance changed. Looks more attractive and the color codes used for buttons.
    5. Padding is added.
    And few more changes.

--- calculator.py
+++ calculator.py
@@ -6,89 +6,93 @@ class calculate():
     def __init__(self):
         self.root = Tk()
         self.root.title("Calculator")
-        self.root.geometry("370x220")
+        self.root.geometry("290x260")
 
-        self.resultwindow = Entry(self.root)
-        self.resultwindow.grid(row=0,column=0,columnspan=6)
+        self.root.maxsize(290, 260)
+        self.root.minsize(290, 260)
+        self.root.config(bg="grey")
+
+        self.resultwindow = Entry(self.root,borderwidth=5, relief=SUNKEN)
+        self.resultwindow.grid(row=0,column=0,columnspan=6,pady=5)
         self.resultwindow.config(font=("Arial", 18))
         self.resultwindow.focus_set()  # Sets focus on the input text area
 
         # Buttons
-        self.button1 = Button(self.root, text="1", width=3, command=lambda:self.ins('1'))
+        self.button1 = Button(self.root, text="1", width=3, command=lambda:self.ins('1'),relief=RAISED,bg='light green')
         self.button1.grid(row=1,column=0, padx=3, pady=3)
         self.button1.config(font=("Arial", 18))
 
-        self.button2 = Button(self.root, text="2", width=3, command=lambda:self.ins('2'))
+        self.button2 = Button(self.root, text="2", width=3, command=lambda:self.ins('2'),relief=RAISED,bg='light green')
         self.button2.grid(row=1, column=1, padx=3, pady=3)
         self.button2.config(font=("Arial", 18))
 
-        self.button3 = Button(self.root, text="3", width=3, command=lambda:self.ins('3'))
+        self.button3 = Button(self.root, text="3", width=3, command=lambda:self.ins('3'),relief=RAISED,bg='light green')
         self.button3.grid(row=1, column=2, padx=3, pady=3)
         self.button3.config(font=("Arial", 18))
 
-        self.button4 = Button(self.root, text="4", width=3, command=lambda:self.ins('4'))
+        self.button4 = Button(self.root, text="4", width=3, command=lambda:self.ins('4'),relief=RAISED,bg='light green')
         self.button4.grid(row=2, column=0, padx=3, pady=3)
         self.button4.config(font=("Arial", 18))
 
-        self.button5 = Button(self.root, text="5", width=3, command=lambda:self.ins('5'))
+        self.button5 = Button(self.root, text="5", width=3, command=lambda:self.ins('5'),relief=RAISED,bg='light green')
         self.button5.grid(row=2, column=1, padx=3, pady=3)
         self.button5.config(font=("Arial", 18))
 
-        self.button6 = Button(self.root, text="6", width=3, command=lambda:self.ins('6'))
+        self.button6 = Button(self.root, text="6", width=3, command=lambda:self.ins('6'),relief=RAISED,bg='light green')
         self.button6.grid(row=2, column=2, padx=3, pady=3)
         self.button6.config(font=("Arial", 18))
 
-        self.button7 = Button(self.root, text="7", width=3, command=lambda:self.ins('7'))
+        self.button7 = Button(self.root, text="7", width=3, command=lambda:self.ins('7'),relief=RAISED,bg='light green')
         self.button7.grid(row=3, column=0, padx=3, pady=3)
         self.button7.config(font=("Arial", 18))
 
-        self.button8 = Button(self.root, text="8", width=3, command=lambda:self.ins('8'))
+        self.button8 = Button(self.root, text="8", width=3, command=lambda:self.ins('8'),relief=RAISED,bg='light green')
         self.button8.grid(row=3, column=1, padx=3, pady=3)
         self.button8.config(font=("Arial", 18))
 
-        self.button9 = Button(self.root, text="9", width=3, command=lambda:self.ins('9'))
+        self.button9 = Button(self.root, text="9", width=3, command=lambda:self.ins('9'),relief=RAISED,bg='light green')
         self.button9.grid(row=3, column=2, padx=3, pady=3)
         self.button9.config(font=("Arial", 18))
 
-        self.button0 = Button(self.root, text="0", width=3, command=lambda: self.ins('0'))
+        self.button0 = Button(self.root, text="0", width=3, command=lambda: self.ins('0'),relief=RAISED,bg='light green')
         self.button0.grid(row=4, column=0, padx=3, pady=3)
         self.button0.config(font=("Arial", 18))
 
-        self.button_open = Button(self.root, text="(", width=3, command=lambda: self.ins('('))
+        self.button_open = Button(self.root, text="(", width=3, command=lambda: self.ins('('),relief=RAISED)
         self.button_open.grid(row=4, column=1, padx=3, pady=3)
         self.button_open.config(font=("Arial", 18))
 
-        self.button_close = Button(self.root, text=")", width=3, command=lambda: self.ins(')'))
+        self.button_close = Button(self.root, text=")", width=3, command=lambda: self.ins(')'),relief=RAISED)
         self.button_close.grid(row=4, column=2, padx=3, pady=3)
         self.button_close.config(font=("Arial", 18))
 
         # Operations Buttons
 
-        self.buttonplus = Button(self.root, text="+", width=3, command=lambda:self.ins('+'))
+        self.buttonplus = Button(self.root, text="+", width=3, command=lambda:self.ins('+'),relief=RAISED)
         self.buttonplus.grid(row=1, column=3, padx=3, pady=3)
         self.buttonplus.config(font=("Arial", 18))
 
-        self.buttonminus = Button(self.root, text="-", width=3, command=lambda:self.ins('-'))
+        self.buttonminus = Button(self.root, text="-", width=3, command=lambda:self.ins('-'),relief=RAISED)
         self.buttonminus.grid(row=1, column=4, padx=3, pady=3)
         self.buttonminus.config(font=("Arial", 18))
 
-        self.buttondivide = Button(self.root, text="/", width=3, command=lambda:self.ins('/'))
+        self.buttondivide = Button(self.root, text="/", width=3, command=lambda:self.ins('/'),relief=RAISED)
         self.buttondivide.grid(row=2, column=3, padx=3, pady=3)
         self.buttondivide.config(font=("Arial", 18))
 
-        self.buttonmultiply = Button(self.root, text="*", width=3, command=lambda:self.ins('*'))
+        self.buttonmultiply = Button(self.root, text="*", width=3, command=lambda:self.ins('*'),relief=RAISED)
         self.buttonmultiply.grid(row=2, column=4, padx=3, pady=3)
         self.buttonmultiply.config(font=("Arial", 18))
 
-        self.buttoncancel = Button(self.root, text="C", width=3, command=lambda: self.cancel())
+        self.buttoncancel = Button(self.root, text="C", width=3, command=lambda: self.cancel(),relief=RAISED,bg='#EF5350',fg='white')
         self.buttoncancel.grid(row=3, column=4, padx=3, pady=3)
         self.buttoncancel.config(font=("Arial", 18))
 
-        self.buttondeleteall = Button(self.root, text="Del", width=3, command=lambda: self.delete_all())
+        self.buttondeleteall = Button(self.root, text="Del", width=3, command=lambda: self.delete_all(),relief=RAISED)
         self.buttondeleteall.grid(row=3, column=3, padx=3, pady=3)
         self.buttondeleteall.config(font=("Arial", 18))
 
-        self.buttonresult = Button(self.root, text="=", width=6, command=lambda:self.calculate())
+        self.buttonresult = Button(self.root, text="=", width=7, command=lambda:self.calculate(),relief=RAISED,bg='#FFEE58')
         self.buttonresult.grid(row=4, column=3, padx=3, pady=3, columnspan=2)
         self.buttonresult.config(font=("Arial", 18))
 
